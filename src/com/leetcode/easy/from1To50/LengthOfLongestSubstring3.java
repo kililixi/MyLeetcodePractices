5883187aa03d336d8164a50cfa59cd14b35879b1
hubinchang <hubc@startsi.cn>
2020-12-23 23:05:05 +0800

    'alter'

--- src/com/leetcode/easy/from1To50/LengthOfLongestSubstring3.java
+++ src/com/leetcode/easy/from1To50/LengthOfLongestSubstring3.java
@@ -12,6 +12,7 @@ public class LengthOfLongestSubstring3 {
         char[] chary = s.toCharArray();
         String str = "";
 
+        // O(n^2) indexOf也是一个for循环
         for(int i=0; i<chary.length; i++) {
             int index = str.indexOf( chary[i] );
             if( index > -1 ) {
@@ -31,10 +32,56 @@ public class LengthOfLongestSubstring3 {
         return max;
     }
 
+    public static int lengthOfLongestSubstring2(String s) {
+        if(s == null || "".equals(s)) {
+            return 0;
+        }
+        int count = 0, i = 0, j = 0;
+        int total = s.length();
+        Set<Character> set = new HashSet<>();
+//        set.add(s.charAt(i));
+        while(i < total && j < total) {
+            if(set.contains(s.charAt(j))) {
+                set.remove(s.charAt(i++));
+            } else {
+                set.add(s.charAt(j++));
+                count = Math.max(count, j-i);
+            }
+        }
+
+        return count;
+    }
+
+    public int lengthOfLongestSubstring3(String s) {
+
+        Map<Character, Integer> map= new HashMap<>();
+        int start=0, len=0;
+
+        // abba
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (map.get(c) >= start)
+                    start = map.get(c) + 1;
+            }
+            len = Math.max(len, i-start+1);
+            map.put(c, i);
+        }
+
+        //
+        // 1 a 0
+        // 2 b 1
+        // 3
+        return len;
+    }
+
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         String input = "abcabcbb";
+//        String input = "abcdbc";
 //        String input = "dvdf";
+//        String input = "bbbbbb";
+//        String input = "pwwkew";
         System.out.println(lengthOfLongestSubstring2(input));
         System.out.println(System.currentTimeMillis() - start );
     }
@@ -46,13 +93,30 @@ public class LengthOfLongestSubstring3 {
      * @param s
      * @return
      */
-    public static int lengthOfLongestSubstring2(String s) {
+    public static int lengthOfLongestSubstringOfficial1(String s) {
         int n = s.length();
         int ans = 0;
         for (int i = 0; i < n; i++)
             for (int j = i + 1; j <= n; j++)
                 if (allUnique(s, i, j))
                     ans = Math.max(ans, j - i);
+        return ans;
+    }
+
+    public static int lengthOfLongestSubstringOfficial2(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
         return ans;
     }
 
