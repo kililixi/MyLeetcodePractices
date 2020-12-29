79766983f1e43c9c4dd688d6df9b3a7027a7be98
hubinchang <hubc@startsi.cn>
2020-12-29 18:07:32 +0800

    alter

---
+++ src/com/leetcode/easy/from1To50/ZigZagConversion6.java
@@ -0,0 +1,24 @@
+package com.leetcode.easy.from1To50;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZigZagConversion6 {
+
+    public static String convert(String s, int numRows) {
+        List<StringBuilder> ros = new ArrayList<>(numRows);
+        for(int i=0; i<numRows; i++) {
+            ros.add(new StringBuilder());
+        }
+        int length = s.length();
+
+        for(int i=0; i<length; i++) {
+
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
