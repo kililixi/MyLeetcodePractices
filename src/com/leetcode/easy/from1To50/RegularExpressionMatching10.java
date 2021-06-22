2474dec1c86f8e0d98790d9dc418447cf9c747c2
hubinchang <hubc@startsi.cn>
2021-06-22 20:49:54 +0800

    alter

--- src/com/leetcode/easy/from1To50/RegularExpressionMatching10.java
+++ src/com/leetcode/easy/from1To50/RegularExpressionMatching10.java
@@ -16,13 +16,60 @@ package com.leetcode.easy.from1To50;
  */
 public class RegularExpressionMatching10 {
 
+    Result[][] memo;
+
     public static void main(String[] args) {
 //        String s = "dddfsdfsdf";
 //        String p = "d*fs.fs.f";
-        String s = "fffsdfsdf";
-        String p = "f*fsdfsdf";
+//        String s = "fffsdfsdf";
+//        String p = "f*fsdfsdf";
+        String s = "f";
+        String p = "d";
 
-        System.out.println(isMatch(s, p));
+        System.out.println(new RegularExpressionMatching10().isMatch2(s, p));
+
+    }
+
+    /**
+     * 采用DP（动态规划）
+     * 简单说就是缓存旧的数据
+     * @param text
+     * @param pattern
+     * @return
+     */
+    public boolean isMatch2(String text, String pattern) {
+        // 缓存中间数据
+        memo = new Result[text.length() + 1][pattern.length() + 1];
+        return dp(0, 0, text, pattern);
+    }
+
+    public boolean dp(int i, int j, String text, String pattern) {
+        // 如果之前缓存了该位置的值，直接返回
+        if (memo[i][j] != null) {
+            return memo[i][j] == Result.TRUE;
+        }
+        boolean ans;
+        // 如果到了pattern的结尾
+        if (j == pattern.length()){
+            // 如果也到了text的结尾,说明两者长度一致，匹配结束了，没
+            // 比如 p='f' t='f' , 从0开始，到 i和j都为1，说明匹配结束，返回true，如果pattern结束了，text没结束，说明有text没匹配到，返回false。
+            ans = i == text.length();
+        } else{
+
+            // 在i没到text结尾时，通过判断当前各自的第一个字符是否相等，或者pattern为. 来判断第一个字符是否符合
+            boolean first_match = (i < text.length() &&
+                    (pattern.charAt(j) == text.charAt(i) ||
+                            pattern.charAt(j) == '.'));
+
+            if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                ans = (dp(i, j+2, text, pattern) ||
+                        first_match && dp(i+1, j, text, pattern));
+            } else {
+                ans = first_match && dp(i+1, j+1, text, pattern);
+            }
+        }
+        memo[i][j] = ans ? Result.TRUE : Result.FALSE;
+        return ans;
     }
 
     public static boolean isMatch(String text, String pattern) {
@@ -72,3 +119,9 @@ public class RegularExpressionMatching10 {
         return first_match && noStarMatch(text.substring(1), pattern.substring(1));
     }
 }
+
+
+enum Result {
+    TRUE, FALSE
+}
+
