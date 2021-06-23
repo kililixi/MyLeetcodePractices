6fb2381a635e493c36d082cde9c67a7b1e48dc6a
hubinchang <hubc@startsi.cn>
2021-06-23 20:48:08 +0800

    alter

--- src/com/leetcode/easy/from1To50/RegularExpressionMatching10.java
+++ src/com/leetcode/easy/from1To50/RegularExpressionMatching10.java
@@ -21,28 +21,108 @@ public class RegularExpressionMatching10 {
     public static void main(String[] args) {
 //        String s = "dddfsdfsdf";
 //        String p = "d*fs.fs.f";
-        String s = "fffsdfsdf";
-        String p = "f*fsdfsdf";
-//        String s = "f";
-//        String p = "d";
+//        String s = "fffsdfsdf";
+//        String p = "f*fsdfsdf";
+        String s = "aa";
+        String p = "a*";
+//        String s = "a*a*a*";
+//        String p = "aaaaabc";
         long start = System.currentTimeMillis();
-        System.out.println(new RegularExpressionMatching10().isMatch2(s, p));
+        System.out.println(new RegularExpressionMatching10().isMatch3(s, p));
         System.out.println(System.currentTimeMillis()-start);
 
     }
 
+    /**
+     * DP
+     * https://leetcode-cn.com/problems/regular-expression-matching/solution/zheng-ze-biao-da-shi-pi-pei-by-leetcode-solution/
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch4(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
+
+    /**
+     * 思路：
+     * 假设 dp(i,j) 代表 text[i:] 与pattern[j:] 是否匹配
+     * 然后循环 text 和 pattern
+     * for(text) {
+     *     for(pattern) {
+     *
+     *     }
+     * }
+     * 双重循环就是把每个可能都遍历到，但是又因为有缓存中间的结果，所以计算量不会很大
+     *  <!-- fn(i,j) = match(i, j) + match(i, j-1) -->
+     * @param text
+     * @param pattern
+     * @return
+     */
     public boolean isMatch3(String text, String pattern) {
+        // dp(i,j) 代表 text[i:] 与pattern[j:] 是否匹配
         boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
         dp[text.length()][pattern.length()] = true;
 
+        // 为什么不是从 text.length() -1 开始？
         for (int i = text.length(); i >= 0; i--){
+            // pattern 从最后一位开始
             for (int j = pattern.length() - 1; j >= 0; j--){
+
+                // 判断当前i和j对应的text和pattern是否匹配
                 boolean first_match = (i < text.length() &&
                         (pattern.charAt(j) == text.charAt(i) ||
                                 pattern.charAt(j) == '.'));
+
+                // 在对比到 pattern 为 'x*'时
                 if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                    // 判断 text[i:] 与 pattern[j:] 是否匹配
+                    /**
+                     * 如果
+                     * 1、dp[i][j+2]
+                     * 表示如果 text[i:] 与 pattern[j+2:] 匹配成功
+                     * 忽略pattern前面两个匹配符，这种情况代表匹配了0个字符，比如 s=fsdfsdf p=d*fsdfsdf,即删了你这部分正则也不影响
+                     *
+                     * 2、first_match && dp[i+1][j]
+                     * 表示如果 text[i] 与 pattern[j] 匹配成功，并且text[i+1:] 与 pattern[j:] 也匹配成功
+                     * 这种情况代表匹配了>=1个text, 比如s=fffsdfsdf p=f*fsdfsdf,删除第一个匹配的字符，pattern不变，继续匹配。
+                     *
+                     * 以上两个操作结果有一个true的话，代表符合该正则。
+                     */
                     dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
                 } else {
+                    // 两种情况
+                    // 1 第一次比对
+                    // 2 没有*的时候，就当作noStarMatch模式，一个个字符匹配
                     dp[i][j] = first_match && dp[i+1][j+1];
                 }
             }
@@ -64,7 +144,7 @@ public class RegularExpressionMatching10 {
     }
 
     /**
-     * 其实就是前面那个方法的优化，把截取字符串的操作变成了遍历字符串而已，这个memo数组有什么必要呢
+     * 其实就是前面那个方法的优化，把截取字符串的操作变成了遍历字符串，减少了开销而已，这个memo数组有什么必要呢
      * @param i
      * @param j
      * @param text
